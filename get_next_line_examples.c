b81ee1b4c873f23c7d9d2c5103d6f9b197556ea0
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-10-19 09:18:49 +0300

    Create get_next_line_examples.c

---
+++ get_next_line_examples.c
@@ -0,0 +1,60 @@
+// Example 1.
+
+#include "get_next_line.h"
+
+static void	next_line(char *str, char **line)
+{
+	size_t	i;
+	char	*next_line;
+
+	i = ft_strcspn(str, "\n");
+	next_line = ft_strnew(i);
+	ft_memcpy(next_line, str, i);
+	line[0] = next_line;
+}
+
+static char	*remaining_lines(char *str)
+{
+	size_t i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n')
+		{
+			i++;
+			return (&str[i]);
+		}
+		i++;
+	}
+	return (&str[i]);
+}
+
+int			get_next_line(const int fd, char **line)
+{
+	int				ret;
+	char			*tmp;
+	char			buf[BUFF_SIZE + 1];
+	static char		*file[MAX_FD];
+
+	if (fd < 0 || fd > MAX_FD || !line)
+		return (-1);
+	if (!file[fd])
+	{
+		file[fd] = ft_strnew(0);
+		while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+		{
+			buf[ret] = '\0';
+			tmp = file[fd];
+			file[fd] = ft_strjoin(tmp, buf);
+			ft_strdel(&tmp);
+		}
+		if (ret == -1)
+			return (-1);
+	}
+	if (file[fd][0] == '\0')
+		return (0);
+	next_line(file[fd], line);
+	file[fd] = remaining_lines(file[fd]);
+	return (1);
+}
