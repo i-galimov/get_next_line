29a3b62e1bd61b20a19241a10f9341e6ee7e995b
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-11-13 16:41:36 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,80 @@
+#include "get_next_line.h"
+
+char	*ft_get_read(int fd, char *str, char *buf)
+{
+	int	count;
+
+	count = 1;
+	while (!ft_strchr(str, '\n') && count)
+	{
+		count = read(fd, buf, BUFFER_SIZE);
+		if (count < 0)
+		{
+			free(buf);
+			return (0);
+		}
+		buf[count] = '\0';
+		if (!str)
+			str = ft_substr(buf, 0, count);
+		else
+			str = ft_strjoin(str, buf);
+	}
+	free(buf);
+	return (str);
+}
+
+char	*trim_end(char *str)
+{
+	char	*line;
+	int		len;
+
+	len = 0;
+	while (str[len] && str[len] != '\n')
+		len++;
+	if (str[len] == '\n')
+		len++;
+	line = ft_substr(str, 0, len);
+	return (line);
+}
+
+char	*trim_start(char *str)
+{
+	int		start;
+	char	*ret;
+
+	start = 0;
+	while (str[start] && str[start] != '\n')
+		start++;
+	start++;
+	ret = ft_substr(str, start, ft_strlen(str));
+	free(str);
+	return (ret);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	char		*buf;
+	static char	*str;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (0);
+	buf = malloc(BUFFER_SIZE + 1);
+	if (!buf)
+		return (0);
+	str = ft_get_read(fd, str, buf);
+	if (!str || str[0] == '\0')
+	{
+		free(str);
+		str = 0;
+		return (NULL);
+	}
+	line = trim_end(str);
+	if (!line || line[0] == '\0')
+	{
+		free(line);
+		return (0);
+	}
+	str = trim_start(str);
+	return (line);
+}
