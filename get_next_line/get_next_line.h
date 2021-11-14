8b47afb0a71c7921e5b2aa5e5a0dc9451a915463
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-11-14 14:26:23 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,13 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(const char *s1, const char *s2);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+char	*get_next_line(int fd);
+
+#endif
